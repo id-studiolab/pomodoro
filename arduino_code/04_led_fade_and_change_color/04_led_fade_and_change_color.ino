48b4a39ba59db2bece8287ce5049595dc8e9ac2a
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-02-12 19:04:09 +0100

    add TODO to exercise 4

--- arduino_code/04_led_fade_and_change_color/04_led_fade_and_change_color.ino
+++ arduino_code/04_led_fade_and_change_color/04_led_fade_and_change_color.ino
@@ -5,7 +5,12 @@
    STEPS:
    -Make sure you connected your ChainableLED
    to the right pin on your Seeeduino Lotus
-*/
+
+   TODO:
+   Whith this code, the color of the led changes only when the loop() restart.
+   Can you modify the code so that the color changes whenever you are turning the
+   potentiometer indipendently from the fade animation?
+ */
 
 #include <FastLED.h>
 
@@ -22,25 +27,25 @@ int potPin=A0;
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  //start the led library
+	//start the led library
 	FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
 }
 
 void loop() {
-  float potValue = analogRead(potPin);
-  int hue = map (potValue,0,1024,0,255);
+	float potValue = analogRead(potPin);
+	int hue = map (potValue,0,1024,0,255);
 
 	leds[0].setHue(hue);
 
-  for (float i = 0; i < 255; i ++) {
-		FastLED.setBrightness(i);
-		FastLED.show();
-    delay(10);
-  }
-
-  for (float i = 255; i > 0; i--) {
+	for (float i = 0; i < 255; i++) {
 		FastLED.setBrightness(i);
 		FastLED.show();
 		delay(10);
-  }
+	}
+
+	for (float i = 255; i > 0; i--) {
+		FastLED.setBrightness(i);
+		FastLED.show();
+		delay(10);
+	}
 }
