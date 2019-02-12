a08f94eb858a4fb8a4703881ee149a6dc5c5a0ff
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-02-12 19:14:14 +0100

    commented and cleaned

--- arduino_code/05_led_fade_and_change_color_asynchronous/05_led_fade_and_change_color_asynchronous.ino
+++ arduino_code/05_led_fade_and_change_color_asynchronous/05_led_fade_and_change_color_asynchronous.ino
@@ -5,7 +5,7 @@
    STEPS:
    -make sure you connected your chainable LED
    to the right pin on your seeduino Lotus
-*/
+ */
 
 #include <FastLED.h>
 
@@ -22,37 +22,51 @@ int potPin=A0;
 CRGB leds[NUM_LEDS];
 
 
-// asincronous fading
+// How long is the fade
 int fadeDuration = 2000;
+// Keep track of last time we updated the led brightess
 long lastLedUpdateTime = 0;
+// How many steps from minumum brightess to maximum brightess
 int fadesteps = 40;
+// how often are we supposed to change the color of the Led
 float fadeUpdateInterval=fadeDuration/fadesteps;
+// are we fading towards the maximum brightess (+1) or towards the minumum brightess (-1)
 int fadeDirection = 1;
+// the maximum brightess
 int fadeMax = 255;
+// the minimum brightess
 int fadeMin = 0;
+// how much do we increase the brightess every step
 float fadeAmountXStep = (fadeMax - fadeMin) / fadesteps;
+// the led brightess
 float brightness;
 
 void setup() {
+	//initialize the led
 	FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
 }
 
 void loop() {
 	float potValue = analogRead(potPin);
-  int hue = map (potValue,0,1024,0,255);
+	int hue = map (potValue,0,1024,0,255);
 
 	leds[0].setHue(hue);
 
-  if (millis() - lastLedUpdateTime > fadeUpdateInterval) {
-    brightness = brightness + fadeAmountXStep * fadeDirection;
-    if (brightness > fadeMax || brightness < fadeMin) {
+	// if fadeUpdateInterval has passed, we have to update the brightess of the led
+	if (millis() - lastLedUpdateTime > fadeUpdateInterval) {
+		// update the brightess value
+		brightness = brightness + fadeAmountXStep * fadeDirection;
+		// if we reach the maximum value or the minimum, we reverse the direction
+		if (brightness > fadeMax || brightness < fadeMin) {
 			//make sure that the value is not outside the range
 			brightness = constrain(brightness, fadeMin, fadeMax);
 			//invert the direction
-      fadeDirection *= -1;
-    }
-    lastLedUpdateTime = millis();
-  }
+			fadeDirection *= -1;
+		}
+		lastLedUpdateTime = millis();
+	}
+
+	//update the led color
 	leds[0].setHSV( hue, 255, brightness);
 	FastLED.show();
 }
